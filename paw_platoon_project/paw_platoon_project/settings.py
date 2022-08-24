2d4dbe1d5ab34f5a9ff0d745e271004a99aaf65b
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-08-24 17:07:20 -0500

    changed back debug settings

--- paw_platoon_project/paw_platoon_project/settings.py
+++ paw_platoon_project/paw_platoon_project/settings.py
@@ -24,11 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--9z=sjf9h6(p_%2q0^r5(h398!$aa2w7*&192)5lmkxu=vmwbi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
-CSRF_TRUSTED_ORIGINS = ['https://paw-platoon.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
